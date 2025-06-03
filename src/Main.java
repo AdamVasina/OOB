ef3200ece21562501129fe305e975aa7c8013f99
AdamVasina <adamvasina@gmail.com>
2025-06-03 21:04:25 +0200

    OOP FIRST

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        RodinnyDom domNovakov = new RodinnyDom();
+        domNovakov.pocetPoschodi = 4;
+        domNovakov.rozloha = 457f;
+        domNovakov.material = "mramor";
+        domNovakov.vypocetCenyDomu(300, "Novakovi");
+
+
+
+        RodinnyDom domZajacov = new RodinnyDom();
+        domZajacov.rozloha = 646f;
+        domZajacov.material ="beton";
+        domZajacov.pocetPoschodi = 3;
+        domZajacov.vypocetCenyDomu(250, "Zajacovi");
+
+
+
+
+
+
+
 
 
 
---
+++ src/RodinnyDom.java
@@ -0,0 +1,17 @@
+public class RodinnyDom {
+    int pocetPoschodi;
+    String material;
+    float rozloha;
+    int pocetOkien;
+
+    public void vypocetCenyDomu(int cenaZaMeter,String menoRodiny){
+        float cena = rozloha * cenaZaMeter * pocetPoschodi;
+        System.out.println(menoRodiny + " " + (rozloha * 300 * pocetPoschodi));
+
+    }
+
+
+
+
+
+}
